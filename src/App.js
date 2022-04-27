65c9b82e1fedfcb36bba0135c11a799f99dae80c
GRECO <greco@air-de-greco-2.lan>
2022-04-27 16:13:00 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [active, setActive] = useState(0);
+  const [noway, setNoway] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Ready To Go</h1>
       </header>
+
+      <div className="all-buttons">
+        <div className="button1">
+          <button
+            // style={{ backgroundColor: active ? "#5C48D3" : "grey"  }}
+            onClick={() => {
+              setActive();
+            }}
+          >
+            OFF
+          </button>
+
+          <button
+            onClick={() => {
+              setActive();
+            }}
+          >
+            ON
+          </button>
+        </div>
+        <div className="button2">
+          <button
+            onClick={() => {
+              setActive();
+            }}
+          >
+            OFF
+          </button>
+
+          <button
+            onClick={() => {
+              setActive();
+            }}
+          >
+            ON
+          </button>
+        </div>
+
+        <div className="button3">
+          <button
+            onClick={() => {
+              setActive();
+            }}
+          >
+            OFF
+          </button>
+
+          <button
+            onClick={() => {
+              setActive();
+            }}
+          >
+            ON
+          </button>
+        </div>
+      </div>
+
+      <div className="Noway">
+        <button
+          onClick={() => {
+            setNoway();
+          }}
+        >
+          No way !
+        </button>
+      </div>
     </div>
   );
 }
